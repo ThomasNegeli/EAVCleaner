948316af9e9c798c857dc696472cc0df803716d4
Thomas Negeli <Thomas.Negeli@cyberhouse.at>
2017-07-12 10:02:31 +0200

    Some reformat and updated .gitignore

--- app/code/EAVCleaner/Console/Command/RemoveUnusedMediaCommand.php
+++ app/code/EAVCleaner/Console/Command/RemoveUnusedMediaCommand.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Hackaton\EAVCleaner\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
@@ -10,69 +11,70 @@ use Magento\Framework\App\Filesystem\DirectoryList;
 
 class RemoveUnusedMediaCommand extends Command
 {
+
     /**
      * Init command
      */
-    protected function configure()
+    protected function configure ()
     {
         $this
-            ->setName('eav:media:remove-unused')
-            ->setDescription('Remove unused product images')
-            ->addOption('dry-run');
+            ->setName( 'eav:media:remove-unused' )
+            ->setDescription( 'Remove unused product images' )
+            ->addOption( 'dry-run' );
     }
 
     /**
      * Execute Command
      *
-     * @param InputInterface $input
+     * @param InputInterface  $input
      * @param OutputInterface $output
      *
      * @return void;
      */
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute ( InputInterface $input, OutputInterface $output )
     {
         $filesize = 0;
         $countFiles = 0;
-        $isDryRun = $input->getOption('dry-run');
+        $isDryRun = $input->getOption( 'dry-run' );
 
-        if(!$isDryRun) {
-            $output->writeln('WARNING: this is not a dry run. If you want to do a dry-run, add --dry-run.');
-            $question = new ConfirmationQuestion('Are you sure you want to continue? [No] ', false);
-            $this->questionHelper = $this->getHelper('question');
-            if (!$this->questionHelper->ask($input, $output, $question)) {
+        if (!$isDryRun) {
+            $output->writeln( 'WARNING: this is not a dry run. If you want to do a dry-run, add --dry-run.' );
+            $question = new ConfirmationQuestion( 'Are you sure you want to continue? [No] ', false );
+            $this->questionHelper = $this->getHelper( 'question' );
+            if (!$this->questionHelper->ask( $input, $output, $question )) {
                 return;
             }
         }
 
         $table = array();
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $filesystem = $objectManager->get('Magento\Framework\Filesystem');
-        $directory = $filesystem->getDirectoryRead(DirectoryList::MEDIA);
+        $filesystem = $objectManager->get( 'Magento\Framework\Filesystem' );
+        $directory = $filesystem->getDirectoryRead( DirectoryList::MEDIA );
         $imageDir = $directory->getAbsolutePath() . DIRECTORY_SEPARATOR . 'catalog' . DIRECTORY_SEPARATOR . 'product';
-        $resource = $objectManager->get('Magento\Framework\App\ResourceConnection');
-        $mediaGallery = $resource->getConnection()->getTableName('catalog_product_entity_media_gallery');
-        $coreRead = $resource->getConnection('core_read');
+        $resource = $objectManager->get( 'Magento\Framework\App\ResourceConnection' );
+        $mediaGallery = $resource->getConnection()->getTableName( 'catalog_product_entity_media_gallery' );
+        $coreRead = $resource->getConnection( 'core_read' );
         $i = 0;
-        $directoryIterator = new \RecursiveDirectoryIterator($imageDir);
+        $directoryIterator = new \RecursiveDirectoryIterator( $imageDir );
 
-        foreach (new \RecursiveIteratorIterator($directoryIterator) as $file) {
+        foreach (new \RecursiveIteratorIterator( $directoryIterator ) as $file) {
 
-            if (strpos($file, "/cache") !== false || is_dir($file)) {
+            if (strpos( $file, "/cache" ) !== false || is_dir( $file )) {
                 continue;
             }
 
-            $filePath = str_replace($imageDir, "", $file);
-            if (empty($filePath)) continue;
-            $value = $coreRead->fetchOne('SELECT value FROM ' . $mediaGallery . ' WHERE value = ?', array($filePath));
+            $filePath = str_replace( $imageDir, "", $file );
+            if (empty( $filePath )) continue;
+            $value = $coreRead->fetchOne( 'SELECT value FROM ' . $mediaGallery . ' WHERE value = ?', array( $filePath ) );
             if ($value == false) {
                 $row = array();
                 $row[] = $filePath;
                 $table[] = $row;
-                $filesize += filesize($file);
+                $filesize += filesize( $file );
                 $countFiles++;
                 echo '## REMOVING: ' . $filePath . ' ##';
                 if (!$isDryRun) {
-                    unlink($file);
+                    unlink( $file );
                 } else {
                     echo ' -- DRY RUN';
                 }
@@ -83,9 +85,9 @@ class RemoveUnusedMediaCommand extends Command
 
         $headers = array();
         $headers[] = 'filepath';
-        $this->getHelper('table')
-            ->setHeaders($headers)
-            ->setRows($table)->render($output);
-        $output->writeln("Found " . number_format($filesize / 1024 / 1024, '2') . " MB unused images in $countFiles files");
+        $this->getHelper( 'table' )
+            ->setHeaders( $headers )
+            ->setRows( $table )->render( $output );
+        $output->writeln( "Found " . number_format( $filesize / 1024 / 1024, '2' ) . " MB unused images in $countFiles files" );
     }
 }
--- app/code/EAVCleaner/Console/Command/RestoreUseDefaultValueCommand.php
+++ app/code/EAVCleaner/Console/Command/RestoreUseDefaultValueCommand.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace Hackaton\EAVCleaner\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
-
 
 
 class RestoreUseDefaultValueCommand extends Command
@@ -16,70 +16,68 @@ class RestoreUseDefaultValueCommand extends Command
     /**
      * Init command
      */
-    protected function configure()
+    protected function configure ()
     {
         $this
-            ->setName('eav:attributes:restore-use-default-value')
-            ->setDescription("
-                Restore product's 'Use Default Value' if the non-global value is the same as the global value
-            ")
-            ->addOption('dry-run');
+            ->setName( 'eav:attributes:restore-use-default-value' )
+            ->setDescription( "Restore product's 'Use Default Value' if the non-global value is the same as the global value" )
+            ->addOption( 'dry-run' );
     }
 
     /**
      * Execute Command
      *
-     * @param InputInterface $input
+     * @param InputInterface  $input
      * @param OutputInterface $output
      *
      * @return void;
      */
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute ( InputInterface $input, OutputInterface $output )
     {
-        $isDryRun = $input->getOption('dry-run');
+        $isDryRun = $input->getOption( 'dry-run' );
 
-        if(!$isDryRun) {
-            $output->writeln('WARNING: this is not a dry run. If you want to do a dry-run, add --dry-run.');
-            $question = new ConfirmationQuestion('Are you sure you want to continue? [No] ', false);
+        if (!$isDryRun) {
+            $output->writeln( 'WARNING: this is not a dry run. If you want to do a dry-run, add --dry-run.' );
+            $question = new ConfirmationQuestion( 'Are you sure you want to continue? [No] ', false );
 
-            $this->questionHelper = $this->getHelper('question');
-            if (!$this->questionHelper->ask($input, $output, $question)) {
+            $this->questionHelper = $this->getHelper( 'question' );
+            if (!$this->questionHelper->ask( $input, $output, $question )) {
                 return;
             }
         }
 
         $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
         /** @var \Magento\Framework\App\ResourceConnection $db */
-        $resConnection = $objectManager->get('Magento\Framework\App\ResourceConnection');
+        $resConnection = $objectManager->get( 'Magento\Framework\App\ResourceConnection' );
         $db = $resConnection->getConnection();
         $counts = array();
         $i = 0;
-        $tables = array('varchar', 'int', 'decimal', 'text', 'datetime');
+        $tables = array( 'varchar', 'int', 'decimal', 'text', 'datetime' );
 
         foreach ($tables as $table) {
             // Select all non-global values
-            $fullTableName = $db->getTableName('catalog_product_entity_' . $table);
-            $rows = $db->fetchAll('SELECT * FROM ' . $fullTableName . ' WHERE store_id != 0');
+            $fullTableName = $db->getTableName( 'catalog_product_entity_' . $table );
+            $rows = $db->fetchAll( 'SELECT * FROM ' . $fullTableName . ' WHERE store_id != 0' );
 
             foreach ($rows as $row) {
                 // Select the global value if it's the same as the non-global value
-                $results = $db->fetchAll('SELECT * FROM ' . $fullTableName
+                $results = $db->fetchAll( 'SELECT * FROM ' . $fullTableName
                     . ' WHERE attribute_id = ? AND store_id = ? AND entity_id = ? AND value = ?',
-                    array($row['attribute_id'], 0, $row['entity_id'], $row['value'])
+                    array( $row['attribute_id'], 0, $row['entity_id'], $row['value'] )
                 );
 
-                if (count($results) > 0) {
+                if (count( $results ) > 0) {
                     foreach ($results as $result) {
                         if (!$isDryRun) {
                             // Remove the non-global value
-                            $db->query('DELETE FROM ' . $fullTableName . ' WHERE value_id = ?', $row['value_id']
+                            $db->query( 'DELETE FROM ' . $fullTableName . ' WHERE value_id = ?', $row['value_id']
                             );
                         }
 
-                        $output->writeln('Deleting value ' . $row['value_id'] . ' "' . $row['value'] .'" in favor of ' . $result['value_id']
+                        $output->writeln( 'Deleting value ' . $row['value_id'] . ' "' . $row['value'] . '" in favor of ' . $result['value_id']
                             . ' for attribute ' . $row['attribute_id'] . ' in table ' . $fullTableName
                         );
-                        if (!isset($counts[$row['attribute_id']])) {
+                        if (!isset( $counts[$row['attribute_id']] )) {
                             $counts[$row['attribute_id']] = 0;
                         }
                         $counts[$row['attribute_id']]++;
@@ -87,25 +85,24 @@ class RestoreUseDefaultValueCommand extends Command
                     }
                 }
 
-                $nullValues = $db->fetchOne('SELECT COUNT(*) FROM ' . $fullTableName
-                    . ' WHERE store_id = ? AND value IS NULL', array($row['store_id'])
+                $nullValues = $db->fetchOne( 'SELECT COUNT(*) FROM ' . $fullTableName
+                    . ' WHERE store_id = ? AND value IS NULL', array( $row['store_id'] )
                 );
 
                 if (!$isDryRun && $nullValues > 0) {
-                    $output->writeln("Deleting " . $nullValues ." NULL value(s) from " . $fullTableName);
+                    $output->writeln( "Deleting " . $nullValues . " NULL value(s) from " . $fullTableName );
                     // Remove all non-global null values
-                    $db->query('DELETE FROM ' . $fullTableName
-                        . ' WHERE store_id = ? AND value IS NULL', array($row['store_id'])
+                    $db->query( 'DELETE FROM ' . $fullTableName
+                        . ' WHERE store_id = ? AND value IS NULL', array( $row['store_id'] )
                     );
                 }
             }
 
 
-            if (count($counts)) {
-                $output->writeln('Done');
-            }
-            else {
-                $output->writeln('There were no attribute values to clean up');
+            if (count( $counts )) {
+                $output->writeln( 'Done' );
+            } else {
+                $output->writeln( 'There were no attribute values to clean up' );
             }
 
         }
